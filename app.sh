bfbf281e163f3c671dcd32b17ebabc5bb15cd439
Ricardo Padilha <ricardospadilha@gmail.com>
2015-06-26 18:35:05 -0700

    Compiled with python 2.7.10

--- app.sh
+++ app.sh
@@ -4,11 +4,16 @@ local VERSION="2.4.4"
 local FOLDER="Cheetah-${VERSION}"
 local FILE="${FOLDER}.tar.gz"
 local URL="https://pypi.python.org/packages/source/C/Cheetah/${FILE}"
-local XPYTHON=~/xtools/python2/${DROBO}
+local XPYTHON="${HOME}/xtools/python2/${DROBO}"
+local BASE="${PWD}"
+export QEMU_LD_PREFIX="${TOOLCHAIN}/${HOST}/libc"
 
 _download_tgz "${FILE}" "${URL}" "${FOLDER}"
 pushd "target/${FOLDER}"
-_PYTHON_HOST_PLATFORM="linux-armv7l" LDSHARED="${CC} -shared -Wl,-rpath,/mnt/DroboFS/Share/DroboApps/python2/lib -L${DEST}/lib-5n" "${XPYTHON}/bin/python" setup.py build_ext --force build --force bdist_egg --dist-dir ../..
+LDSHARED="${CC} -shared -Wl,-rpath,/mnt/DroboFS/Share/DroboApps/python2/lib -L${XPYTHON}/lib" \
+  "${XPYTHON}/bin/python" setup.py build_ext \
+  --include-dirs="${XPYTHON}/include" --library-dirs="${XPYTHON}/lib" \
+  --force build --force bdist_egg --dist-dir "${BASE}"
 popd
 }
 
